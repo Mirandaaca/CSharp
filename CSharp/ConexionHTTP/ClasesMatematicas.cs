aefdb511223d1ac8080e758375bd8b3fdcef840e
Cristopher Aramayo <cristopher1012z@gmail.com>
2023-05-31 18:30:21 -1100

    namespaces, using y alcance de variables

---
+++ CSharp/ConexionHTTP/ClasesMatematicas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trigonometricas
+{
+    /*Dentro de las carpetas que creemos dentro de nuestro proyecto, podemos crear multiples archivos.cs
+      y a su vez dentro de esos archivos.cs podemos tener multiples namespaces, que sirven para separar y agrupar otras cosas (como ser clases).
+     */
+    /*
+     En C# las clases no necesariamente deben llevar el mismo nombre del archivo 
+     donde se encuentran las clases, es decir, si nuestro archivo se llama ClasesMatematicas, las clases
+     que se creen dentro no necesariamente deben llamarse ClasesMatematicas. En otro lenguajes como Java si es necesario.
+     */
+    public class Circulo
+    {
+        //Al ser de tipo internal solo se puede usar en ambito local, pero como todo esta dentro del proyecto CSharp, entonces puede ser llamado en otro archivo.cs. 
+        //Es mejor poner las clases de tipo publico.
+    }
+}
+//dentro de archivos.cs puedo tener muchos namespaces, que sirven para agrupar clases, bloques de codigo, funciones, etc.
+namespace Algebraicas
+{
+    public class Sumatorias
+    {
+
+    }
+    //pueden haber anidamientos de namespaces, puede haber grupos dentro de grupos o lo que es mas simple
+    //los namespaces pueden tener otros namespaces, es decir, otras agrupaciones, clases, funciones, bloques de codigo, etc.
+
+    namespace Matriciales
+    { 
+        public class MatrizPerspectiva
+        {
+
+        }
+    }
+}
+
+
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace CSharp
+﻿using Trigonometricas;
+using Algebraicas.Matriciales;
+using Algebraicas;
+
+namespace zona_principal
 {
     internal class Program
     {
+        int var_global = 9;
         static void Main(string[] args)
         {
             /*
@@ -29,22 +34,111 @@
 
             double gravedad = 9.81;
             /*
-             ***************REGLAS DE VARIABLES************
+             ***************REGLAS DE VARIABLES**********
              * un flotante no puede ser guardado en un double
+             * un entero no puede guardar un double
              * un double puede guardar un flotante
              * un entero no puede guardar un flotante
              * un flotante puede guardar un entero
              */
-            double micro = peso;
+            double micro = peso + gravedad / 0;
+
             float x2 = 3.1415f;
 
             Console.WriteLine(x2);
 
+            // CASTEO - truncar con (int) un numero, es decir, reducir decimales
             int res = (int)x2;
 
             Console.WriteLine(res);
 
             Console.WriteLine("Hola Mundo!");
+
+            string apellido = "Perez";
+
+            //Tipos de variable var
+            var nombre = "Pepe";
+
+            var edad = 20;
+
+            var w = 56.7;
+
+            string numeroEnTexto = "25";
+
+            edad = int.Parse(numeroEnTexto);
+
+
+
+            Console.WriteLine(edad);
+            /*
+             Existe tres tipos de errores:
+            *Sintaxis = no cumplir con las nomas de tipado o escritura del lenguaje
+            *Compilacion/Ejecucion 
+            *Semantico
+             */
+            /*
+             Los namespaces sirven para agrupar clases o contenido.
+             El primer agrupador de los namespaces es el proyecto el cual agrupa contenido, folders, programs.cs, etc.
+             */
+
+            //Usando clases propias
+
+            Circulo c;
+            MatrizPerspectiva matriz1;
+            Sumatorias sum;
+
+            //Podemos llamar a la clase directamente sin ocupar el using namespace para asignarlo a una variable
+            //De esta forma el namespace no sera ocupado en tooodo el program.cs y solo afectara a esa variable, de esta forma no ocupamos tanto proceso o memoria.
+
+            Algebraicas.Sumatorias sums;
+            Algebraicas.Matriciales.MatrizPerspectiva mtriz;
+            otro.una_clase cc;
+
+            //Operaciones
+
+            Console.WriteLine("Ingrese un numero (diferente a cero)");
+
+            //Manejo de excepciones
+
+            string valorDelUsuario = Console.ReadLine();
+            int numeroMisterioso = int.Parse(valorDelUsuario);
+            int x = 0;
+            //try = intentar ejecutar esa linea, si lo logra se sale del bloque y si no lo logra pasara a la exepcion, entrara en el catch
+            try
+            {
+               x = 50 / numeroMisterioso;
+            } catch (DivideByZeroException e) //si no logra ejecutar la linea dentro del bloque try, entonces ejecutara la linea del catch, es como in if/else
+            {
+                Console.WriteLine("Por favor, intente de nuevo, usted ingreso el numero 0.");
+            } //una vez termine de ejecutarse el catch, seguira con el codigo, el programara continuara
+
+
+            Console.WriteLine("La division es: " + x);
+
+        }
+        public void metodito()
+        {
+            int x = 3 * var_global;
+        }
+    }
+}
+
+namespace otro
+{
+    //dentro de los namespace solo pueden haber clases, namespaces y usings.
+    using System.Collections.Generic; //Sera usado para todas las clases o bloques de codigo del namespace otro. Es de uso local
+    public class una_clase
+    {
+        //dentro de las clases pueden haber metodos y variables, pero no calculos
+        Circulo circulo2;
+        int y = 9;
+        //y = 7; solo se puede declarar variables, mas no reasignar o hacer operaciones con las variables, eso solo se hace en los metodos de la clase
+        //Se puede usar la clase Circulo porque su namespace Trigonometricas esta siendo utilizada o llamada arriba de todo
+        public void metodo()
+        {
+            int y = 9;
+            y = 7;
+            y = 7 * 3;
         }
     }
 }
